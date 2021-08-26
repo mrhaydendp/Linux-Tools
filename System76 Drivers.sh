3557e855c6bfd50c091f416c4d37afc6b733628d
Hayden <mrhaydendp@gmail.com>
2021-08-25 18:26:48 -0700

    Create System76 Drivers.sh

---
+++ System76 Drivers.sh
@@ -0,0 +1,10 @@
+!bin/bash
+
+# Download system76-apt-preferences & Move to Correct Directory
+wget https://github.com/mrhaydendp/Linux-Setup-Tools/raw/main/system76-apt-preferences
+sudo mv system76-apt-preferences /etc/apt/preferences.d/
+
+# Install Driver App
+sudo apt-add-repository -y ppa:system76-dev/stable
+sudo apt-get update
+sudo apt install system76-driver
