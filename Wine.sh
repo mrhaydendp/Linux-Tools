1982e3181fd156f0d793ded4fa6bc9fa7f5f19c4
Hayden <mrhaydendp@gmail.com>
2021-08-25 18:17:48 -0700

    Create Wine.sh

---
+++ Wine.sh
@@ -0,0 +1,19 @@
+!bin/Bash
+
+# Wine Installer
+codename=$(lsb_release -cs)
+
+# Add 32-bit Architecture
+sudo dpkg --add-architecture i386
+
+# Download Repository Key
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+
+# Add Repository Key
+sudo apt-key add winehq.key
+
+# Add Repository + Change Codename to Current Distro
+echo sudo add-apt-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $codename"
+
+# Update Repos && Install Wine
+sudo apt update && sudo apt install --install-recommends winehq-stable
