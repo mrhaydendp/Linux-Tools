66d9d4495b5faed3ba8b26d5d6f4a7ea0780a1dd
Hayden <mrhaydendp@gmail.com>
2021-09-10 08:57:16 -0700

    Update speedtest-avg.sh

--- Scripts/speedtest-avg.sh
+++ Scripts/speedtest-avg.sh
@@ -1,3 +1,4 @@
+# Check if Speedtest-cli is installed
 if [ $(which speedtest == '*speedtest') ]; then
     echo Speetest-cli is installed
 else
@@ -5,6 +6,7 @@ else
     sudo apt install speedtest-cli -y
 fi
 
+# Run speedtest & output, wait 30 seconds
 speedtest > run1.txt
 
 sleep 30
@@ -15,12 +17,16 @@ sleep 30
 
 speedtest > run3.txt
 
+# Get download speeds
 arr=( $(cat run* | grep "Download:") )
 
+# Download speed results 
 echo "Download Speed (Mbps):"
 echo "(${arr[1]} + ${arr[4]} + ${arr[7]}) / 3" | bc -l | awk '{printf("%.2f\n",$1 + 0.5)}'
 
+# Get upload speeds
 arr=( $(cat run* | grep "Upload:") )
 
+# Upload speed results
 echo "Upload Speed (Mbps):"
 echo "(${arr[1]} + ${arr[4]} + ${arr[7]}) / 3" | bc -l | awk '{printf("%.2f\n",$1 + 0.5)}'
