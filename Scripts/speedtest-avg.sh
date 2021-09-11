7b230f66706a1fd08add31998e04c08b0e444ee6
Hayden Plumley <mrhaydendp@gmail.com>
2021-09-11 11:54:07 -0700

    Update speedtest-avg.sh
    - Fixed ping averages errors
    - Removed -y argument for security reasons

--- Scripts/speedtest-avg.sh
+++ Scripts/speedtest-avg.sh
@@ -4,8 +4,8 @@
 if [ $(which speedtest == '*speedtest') ]; then
     echo Speedtest-cli is Installed
 else
-    echo Speedtest-cli is Not Installed, Installing Now
-    sudo apt install speedtest-cli -y
+    echo Speedtest-cli is Not Installed
+    sudo apt install speedtest-cli
 fi
 
 # Run speedtest & output, wait 30 seconds
@@ -20,11 +20,11 @@ sleep 30
 speedtest-cli > run3.txt
 
 # Get ping
-arr=( $(cat run* | grep "ms") )
+arr=( $(cat run* | grep "ms" | awk -F ':' '{print $2}') )
 
 # Average ping
 echo "Average Ping (ms):"
-echo "(${arr[8]} + ${arr[18]} + ${arr[28]}) / 3" | bc -l | awk '{printf("%.2f\n",$1 + 0.5)}'
+echo "(${arr[0]} + ${arr[2]} + ${arr[4]}) / 3" | bc -l | awk '{printf("%.2f\n",$1 + 0.5)}'
 
 # Get download speed
 arr=( $(cat run* | grep "Download:") )
