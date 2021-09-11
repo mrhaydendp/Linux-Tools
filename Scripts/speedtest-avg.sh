9bc423d9a0d72f2fa1747fc91da60c21a3ffead2
Hayden Plumley <mrhaydendp@gmail.com>
2021-09-11 10:08:36 -0700

    Update speedtest-avg.sh
    - Corrected spelling mistake
    - Edited comments
    - Added average ping

--- Scripts/speedtest-avg.sh
+++ Scripts/speedtest-avg.sh
@@ -2,33 +2,40 @@
 
 # Check if Speedtest-cli is installed
 if [ $(which speedtest == '*speedtest') ]; then
-    echo Speetest-cli is Installed
+    echo Speedtest-cli is Installed
 else
     echo Speedtest-cli is Not Installed, Installing Now
     sudo apt install speedtest-cli -y
 fi
 
 # Run speedtest & output, wait 30 seconds
-speedtest > run1.txt
+speedtest-cli > run1.txt
 
 sleep 30
 
-speedtest > run2.txt
+speedtest-cli > run2.txt
 
 sleep 30
 
-speedtest > run3.txt
+speedtest-cli > run3.txt
 
-# Get download speeds
+# Get ping
+arr=( $(cat run* | grep "ms") )
+
+# Average ping
+echo "Average Ping (ms):"
+echo "(${arr[8]} + ${arr[18]} + ${arr[28]}) / 3" | bc -l | awk '{printf("%.2f\n",$1 + 0.5)}'
+
+# Get download speed
 arr=( $(cat run* | grep "Download:") )
 
-# Download speed results 
+# Average download speed
 echo "Average Download Speed (Mbps):"
 echo "(${arr[1]} + ${arr[4]} + ${arr[7]}) / 3" | bc -l | awk '{printf("%.2f\n",$1 + 0.5)}'
 
-# Get upload speeds
+# Get upload speed
 arr=( $(cat run* | grep "Upload:") )
 
-# Upload speed results
+# Average upload speed
 echo "Average Upload Speed (Mbps):"
 echo "(${arr[1]} + ${arr[4]} + ${arr[7]}) / 3" | bc -l | awk '{printf("%.2f\n",$1 + 0.5)}'
