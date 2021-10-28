8e0d353a20c17c093e47311f1423b18bd93cb605
Hayden Plumley <mrhaydendp@gmail.com>
2021-10-28 15:46:40 -0700

    Update Proton-GE-Updater.sh
    - Remove Steam launch
    - Added comment to Proton-GE install

--- Proton-GE-Updater.sh
+++ Proton-GE-Updater.sh
@@ -2,8 +2,6 @@
 
 # Check if Steam is Installed
 if [ $(which steam == '*steam') ]; then
-    # Launch Steam and Setup Files
-    steam    
     # Format $ver Variable
     format=$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "name" | grep Proton-* |  cut -d '"' -f 4)
     format=$(echo "${current%.*}")
@@ -12,6 +10,7 @@ if [ $(which steam == '*steam') ]; then
     if [ $(ls ~/.steam/root/compatibilitytools.d/$ver 2> /dev/null) ]; then
         echo "Latest Proton is Installed" 
     else
+        # Install Latest Proton-GE From GitHub Page & Move to .steam/root/compatibilitytools.d/
         echo "Installing Latest Proton"
         curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "browser_download_url.*tar.gz" | cut -d '"' -f 4 | wget -i -
         mkdir ~/.steam/root/compatibilitytools.d
