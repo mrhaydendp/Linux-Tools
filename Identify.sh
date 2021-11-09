af0cbfc28b382c6487d9dc0041f35510d4b8166e
Hayden Plumley <mrhaydendp@gmail.com>
2021-11-09 15:16:50 -0800

    Update Identify.sh
    - Added form factor, vendor, & model

--- Identify.sh
+++ Identify.sh
@@ -19,3 +19,13 @@ populated=$(sudo lshw -class memory | grep "description: SODIMM" | wc -l)
 echo "You Have "$populated" Populated Ram Slots With a Total of:"
 ram=$(free --giga | grep "Mem:" | awk '{print $2}')
 echo ""$ram"GB of Ram"
+
+# Get Device Form Factor
+echo "Form Factor:"
+sudo lshw -c system | grep "description:" | awk '{print $2}'
+
+# Get Device Vendor & Model
+echo "Device:"
+vendor=$(sudo lshw -c system | grep "vendor:" | awk '{print $2}')
+model=$(sudo dmidecode -t system | grep "Product Name:" | awk '{print $3,$4}')
+echo "$vendor" "$model"
