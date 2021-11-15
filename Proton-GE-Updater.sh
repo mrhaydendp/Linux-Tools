110e4282aaefc70b8ba0afddbe6d51847e9e7d18
Hayden Plumley <mrhaydendp@gmail.com>
2021-11-15 11:51:10 -0800

    Code Cleanup & Optimizations
    - Slimmed down code
    - Replaced if statements with bash operators
    - rewrote get latest version function
    - Passed all checks in shellcheck

--- Proton-GE-Updater.sh
+++ Proton-GE-Updater.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
 # Check if Steam is Installed
-if [ $(command -v steam == '*steam') ]; then
-    # Format $ver Variable
-    format=$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "name" | grep Proton-* |  cut -d '"' -f 4)
-    format=$(echo "${current%.*}")
-    ver=$(echo "${current%.*}")
+[ -z "$(command -v steam)" ] &&
+    echo "Steam Isn't Installed" && exit
+[ -z "$(command -v steam)" ] ||
+    # Get Latest Proton Version
+    latest=$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep -m 1 Proton |  cut -d '"' -f 4 | awk '{print $1}') &&
     # Check if Latest Proton is Installed
-    if [ $(ls ~/.steam/root/compatibilitytools.d/$ver 2> /dev/null) ]; then
-        echo "Latest Proton is Installed" 
-    else
+    [ -z "$(find ~/.steam/root/compatibilitytools.d/"$latest" 2> /dev/null)" ] ||
+        echo "Latest Proton is Installed"
+    [ -z "$(find ~/.steam/root/compatibilitytools.d/"$latest" 2> /dev/null)" ] &&
         # Install Latest Proton-GE From GitHub Page & Move to .steam/root/compatibilitytools.d/
-        echo "Installing Latest Proton"
-        curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "browser_download_url.*tar.gz" | cut -d '"' -f 4 | wget -i -
-        mkdir ~/.steam/root/compatibilitytools.d
-        tar -xf *.tar.gz -C ~/.steam/root/compatibilitytools.d/ && rm *.tar.gz
+        echo "Installing Latest Proton" &&
+        curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "browser_download_url.*tar.gz" | cut -d '"' -f 4 | wget -i - &&
+        mkdir ~/.steam/root/compatibilitytools.d 2> /dev/null
+        tar -xf ./*.tar.gz -C ~/.steam/root/compatibilitytools.d/ && rm ./*.tar.gz &&
         echo "Successfully Installed"
-    fi
-else
-    echo "Steam not Installed"
-    exit
-fi
