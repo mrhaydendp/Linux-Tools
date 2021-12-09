1e9155d45a47eb74db748c477c7080580d3e82fc
Hayden Plumley <mrhaydendp@gmail.com>
2021-12-09 12:38:44 -0800

    QOL Changes
    - Remade speedtest check
    - The script now quits if one of the speedtests encounter an error
    - Cleaned up output so easier to read
    - Added links to view speedtest results in the browser

--- speedtest-avg.sh
+++ speedtest-avg.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # Check if Speedtest-CLI is Installed
-[ -z != $(command -v speedtest) ] ||
-    { echo "Speedtest-CLI is Not Installed"; exit; } &&
+if ! command -v speedtest &> /dev/null; then
+    echo "Speedtest is Not Installed"; exit
+else
     echo "Running Speedtests..."
+fi
 
 # Run 3 Speedtests & Output to Run*.txt w/ Progress Hashes 
 for i in 1 2 3
@@ -11,22 +13,31 @@ do
     [ "$i" = 1 ] && echo -ne '                          (0%)\r'
     [ "$i" = 2 ] && echo -ne '#####                     (33%)\r'
     [ "$i" = 3 ] && echo -ne '#############             (66%)\r'
-    speedtest-cli > run$i.txt
+    speedtest --share > run$i.txt ||
+    { echo "One or More of the Speedtests Failed, Exiting"; exit; }
 done
 echo -ne '#######################   (100%)\r'
-echo -ne '\n'
+echo -e '\n'
 
 # Calculate Average Ping Time & Display
-arr=($(grep "ms" ./run*.txt | awk -F ':' '{print $3}')) &&
+arr=($(grep "ms" ./run*.txt | awk -F ':' '{print $3}'))
 Ping=$(echo "(${arr[0]} + ${arr[2]} + ${arr[4]}) / 3" | bc -l | awk '{printf("%.2f\n",$1)}')
 echo "Average Ping: $Ping ms"
 
 # Calculate Average Download Speed & Display
-arr=( $(grep "Download:" ./run*.txt) )
+arr=($(grep "Download:" ./run*.txt)) &&
 Download=$(echo "(${arr[1]} + ${arr[4]} + ${arr[7]}) / 3" | bc -l | awk '{printf("%.2f\n",$1)}')
 echo "Average Download Speed: $Download Mbps"
 
 # Calculate Average Upload Speed & Display
-arr=( $(grep "Upload:" ./run*.txt) )
+arr=($(grep "Upload:" ./run*.txt)) &&
 Upload=$(echo "(${arr[1]} + ${arr[4]} + ${arr[7]}) / 3" | bc -l | awk '{printf("%.2f\n",$1)}')
-echo "Average Upload Speed: $Upload Mbps"
+echo -e "Average Upload Speed: $Upload Mbps \n"
+
+# View Results on the Web
+echo "View Results in the Browser:"
+arr=($(grep "http://" ./run*.txt | awk '{print $3}' | cut -f 3 -d "."))
+for http in "${arr[@]}"
+do
+    echo "- https://www.speedtest."${http}
+done
