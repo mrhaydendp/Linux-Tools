9da053064926c5127acc7877a90c1b24ee52a824
Hayden Plumley <mrhaydendp@gmail.com>
2021-12-20 10:50:39 -0800

    Update Identify.sh
    - Changed shebang to `#!/usr/bin/env bash` for better compatibility
    - Changed output format so its cleaner and is set up for more categories
    - Slimmed down & optimized code

--- Identify.sh
+++ Identify.sh
@@ -1,33 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Software
+echo -e "Software: \n"
 
 # Get Hostname
-echo "Your Hostname Is:"
-hostname
+echo "Hostname: $(hostname)"
 
 # Get IP Address
-echo "Your IP Address Is:"
-hostname -I | awk '{print $1}'
-
-# Get Cpu Info
-echo "Your Processor Is:"
-sudo lshw -class processor | grep "product:" | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}'
+echo "Ip Address: $(hostname -I | awk '{print $1}')"
 
 # Get Distro Name
-echo "You Are Running:"
-lsb_release -ds
+echo -e "Distro: $(lsb_release -ds) \n"
+
+# Hardware
+echo -e "Hardware: \n"
 
 # Get Populated Dimm Slots & Total Memory
 populated=$(sudo lshw -class memory | grep -c "description: SODIMM")
-echo You Have "$populated" Populated Ram Slots With a Total of:
-ram=$(free --giga | grep "Mem:" | awk '{print $2}')
-echo "$ram"GB of Ram
+per=$(echo "$(free --giga | grep "Mem:" | awk '{print $2}') / $(sudo lshw -class memory | grep -c "description: SODIMM")" | bc)
+echo Ram: "$populated"x "$per"GB
+
+# Get Cpu Info
+echo "Processor: $(sudo lshw -class processor | grep "product:" | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}')"
 
 # Get Device Form Factor
-echo "Form Factor:"
-sudo lshw -c system | grep "description:" | awk '{print $2}'
+echo "Form Factor: $(sudo lshw -c system | grep "description:" | awk '{print $2}')"
 
 # Get Device Vendor & Model
-echo "Device:"
 vendor=$(sudo lshw -c system | grep "vendor:" | awk '{print $2}')
 model=$(sudo dmidecode -t system | grep "Product Name:" | awk '{print $3,$4}')
-echo "$vendor" "$model"
+echo "Device: $vendor $model"
