d64e3e2c066cec12e93f0983ac790d65d0a9df55
Hayden Plumley <mrhaydendp@gmail.com>
2022-01-22 10:00:52 -0800

    Update Wine-Installer.sh
    - Updated shebang to `#!/usr/bin/env bash` for better practice & compatibility 
    - Replaced codename variable for inline command
    - Replaced `lsb_release` with `grep /etc/os-release` because its more common and works on distros with custom version names
    - Added support for Elementary OS

--- Wine-Installer.sh
+++ Wine-Installer.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Check if Wine is Installed
 [ "$(command -v wine)" = "/usr/bin/wine" ] &&
     echo "Wine is Already Installed" && exit
 [ "$(command -v wine)" = "/usr/bin/wine" ] ||
     echo "Installing Wine" &&
-    # Get Distros Codename
-    codename=$(lsb_release -cs) &&
     # Add 32-bit Architecture
     sudo dpkg --add-architecture i386 &&
     # Download Repository Key
     wget -nc https://dl.winehq.org/wine-builds/winehq.key &&
     # Add Repository Key
     sudo apt-key add winehq.key &&
-    # Add Repository + Change Codename to Current Distro
-    sudo add-apt-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $codename main" &&
+    # Add Repository + Change Codename to Current Distros Ubuntu Codename
+    sudo add-apt-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $(grep "UBUNTU" /etc/os-release | cut -f 2 -d "=") main" &&
     # Install Wine
     sudo apt install --install-recommends winehq-stable -y &&
-    echo 'Successfuly Installed Wine'
+    echo "Successfuly Installed Wine"
